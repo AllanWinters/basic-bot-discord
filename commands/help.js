fed40b787a40da1154dd01a2497d602c3e4d5a13
AllanWinters <70565234+AllanWinters@users.noreply.github.com>
2021-02-26 15:14:05 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,10 @@ const { MessageEmbed } = require('discord.js')
 
 exports.run = async (client, message) => {
   let embed = new MessageEmbed()
-  .setTitle('┏╋━━━ ◥◣◆ Help Commands ◆◢◤ ━━━╋┓')
+  .setTitle('┏╋━━━ ◥◣◆ Hel')
   .setColor('#0086FF')
-  .setDescription('‎\n**__Ini adalah beberapa command yang tersedia__**\n\n> **CORE**\n`help` | `bot` | `ping`\n\n> **RULES**\n`rulec1` | `rulec2` | `rulec3`\n`rulec4` | `rulec5` | `rulec6`') //ini cuma contoh
+  .setDescription('')
+  .addField('')
   .setTimestamp()
   .setFooter('MINEPLAY')
   message.channel.send(embed)
